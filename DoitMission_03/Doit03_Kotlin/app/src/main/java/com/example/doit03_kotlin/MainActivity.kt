0c714e1fedb7f2068875b0f1103919387c975bc8
hkd0694 <gksrudehd123@naver.com>
2019-11-20 11:59:15 +0900

    Doit03_Kotlin : 마루리

---
+++ DoitMission_03/Doit03_Kotlin/app/src/main/java/com/example/doit03_kotlin/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.doit03_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageButton2.setOnClickListener {
+            topimage.visibility = View.VISIBLE
+            if(bottomimage.visibility == View.VISIBLE)
+                bottomimage.visibility = View.GONE
+        }
+
+        imageButton1.setOnClickListener {
+            bottomimage.visibility = View.VISIBLE
+            if (topimage.visibility == View.VISIBLE)
+                topimage.visibility = View.GONE
+        }
+    }
+}
