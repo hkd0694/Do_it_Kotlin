5e4d2463402362fed94c48c9728c1d65846dcb48
hkd0694 <gksrudehd123@naver.com>
2019-11-22 15:18:17 +0900

    Doit04_Java : activity_main.xml 수정

--- DoitMission_04/Doit04_Java/app/src/main/java/com/example/doit04_java/MainActivity.java
+++ DoitMission_04/Doit04_Java/app/src/main/java/com/example/doit04_java/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.doit04_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
