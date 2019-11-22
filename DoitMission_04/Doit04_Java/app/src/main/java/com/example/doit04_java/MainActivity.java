be1e24a88d0a9a61ba3c91f57a5e0849afe1e6a4
hkd0694 <gksrudehd123@naver.com>
2019-11-22 15:57:33 +0900

    Doit04_Java : finish

--- DoitMission_04/Doit04_Java/app/src/main/java/com/example/doit04_java/MainActivity.java
+++ DoitMission_04/Doit04_Java/app/src/main/java/com/example/doit04_java/MainActivity.java
@@ -3,14 +3,62 @@ package com.example.doit04_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button trans;
+    private Button close;
+    private EditText edittext;
+    private TextView textByte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        trans = findViewById(R.id.transbtn);
+        close = findViewById(R.id.closebtn);
+        edittext = findViewById(R.id.edittext);
+        textByte = findViewById(R.id.textbyte);
+
+        edittext.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textByte.setText(String.valueOf(count));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        trans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),edittext.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textByte.setText("0");
+                edittext.setText("");
+            }
+        });
     }
 }
