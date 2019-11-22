ef7cbc6c2b5651568367776a7c8b22bdc9f97522
hkd0694 <gksrudehd123@naver.com>
2019-11-22 16:25:40 +0900

    Doit04_Kotlin : finish

--- DoitMission_04/Doit04_Kotlin/app/src/main/java/com/example/doit04_kotlin/MainActivity.kt
+++ DoitMission_04/Doit04_Kotlin/app/src/main/java/com/example/doit04_kotlin/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.doit04_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        edittext.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                textbyte.text = count.toString()
+            }
+
+        })
+
+
+        transbtn.setOnClickListener {
+            toast(edittext.text)
+        }
+
+        closebtn.setOnClickListener {
+            edittext.setText("")
+            textbyte.text = "0"
+        }
     }
 }
