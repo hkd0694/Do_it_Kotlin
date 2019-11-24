8358ed25df669636da0676076c89d1cdc5f078fe
hkd0694 <gksrudehd123@naver.com>
2019-11-24 21:31:45 +0900

    Doit08_Kotlin : initial commit

---
+++ DoitMission_08/Doit08_Kotlin/app/src/main/java/com/example/doit08_kotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.doit08_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
